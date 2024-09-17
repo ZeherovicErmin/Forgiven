af9cc2ccfc17af796a35010bcedcabbd046b3fe5
ZeherovicErmin <ZeherovicErmin@gmail.com>
2024-09-16 22:49:46 -0400

    Added table & text prompt
    need to finish text but learned quite a bit about importing models. need to learn how to animate / rig

---
+++ Assets/Scripts/Interactables/WoodTable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WoodTable : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+        UnityEngine.Debug.Log("Interacted with " + gameObject.name);
+    }
+}
